2d76b0a0dfef60286e87631e0136bb6c07b5f19d
andrew <andrew@dah.mint.box>
2013-10-05 22:56:43 -0700

    Adding UI

---
+++ your_server/your_character/SavedVariables/Blizzard_TimeManager.lua
@@ -0,0 +1,7 @@
+
+BlizzardStopwatchOptions = {
+	["position"] = {
+		["y"] = 773.4830932617188,
+		["x"] = 443.906982421875,
+	},
+}
---
+++ your_server/your_character/SavedVariables/Recount.lua
@@ -0,0 +1,22 @@
+
+RecountPerCharDB = {
+	["version"] = "1.3",
+	["combatants"] = {
+		["Ayumez"] = {
+			["Fights"] = {
+			},
+			["type"] = "Self",
+			["Name"] = "Ayumez",
+			["UnitLockout"] = 1332496178,
+			["level"] = 85,
+			["Owner"] = false,
+			["enClass"] = "WARLOCK",
+			["LastFightIn"] = 0,
+		},
+	},
+	["FightNum"] = 0,
+	["CombatTimes"] = {
+	},
+	["FoughtWho"] = {
+	},
+}
